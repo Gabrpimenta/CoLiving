e9ba6f969846d565d4737bf8408f01f1300d21a4
Gabriel Pimenta <gabriel.pimenta@superlogica.com>
2025-12-08 14:26:20 -0300

    Update App component and package dependencies
    - Reintroduced AppNavigator import in App.tsx for navigation functionality.
    - Updated package.json to downgrade @react-native-community/netinfo and @react-navigation/native to specific versions, and added new dependencies for bottom and material top tabs navigation.
    - Adjusted yarn.lock to reflect the changes in package versions and added new dependencies for navigation.

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import { ApolloProvider } from '@apollo/client/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { apolloClient } from '@/config/apolloClient';
-import { AppNavigator } from '@/navigation/AppNavigator';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import { AppNavigator } from '@/navigation/AppNavigator';
 
 export default function App(): React.JSX.Element {
   return (
